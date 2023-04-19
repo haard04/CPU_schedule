11453d0aab8bff9e87ba50bd20126402690b2572
haard04 <haardshah04@gmail.com>
2023-04-19 21:19:28 +0530

    API

--- cpu/lib/openai/constants.dart
+++ cpu/lib/openai/constants.dart
@@ -1,5 +1,5 @@
-const key1= "sk-Z1SBA7x";
-const key2= "K6AYMur1td";
-const key3= "JSdT3Blbk";
-const key4= "FJH8F9ElH6";
-const key5= "4sv4ytWqB9kG";
+const key1= "sk-jq3ydZx";
+const key2= "PEIdWst5";
+const key3= "WCwhnT3Blb";
+const key4= "kFJQPUXs7wq";
+const key5= "CzoIt818QQGp";
